c7df4f4371a78e5ce7814165812ba68505ca44d4
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-04 11:56:27 +0300

    Fix remove function

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ import './style.scss';
 
 const tasks = [
   {
-    description: "taskDescription oeshjfgslkfglsdfjg;lsk j",
+    description: "one",
     completed: false,
     index: 0,
 },
 {
-  description: "taskDescription",
+  description: "2",
   completed: false,
   index: 1,
 },
 {
-  description: "taskDescription",
+  description: "3",
   completed: false,
   index: 1,
 },
 {
-  description: "taskDescription",
+  description: "4",
   completed: false,
   index: 1,
 },
@@ -45,13 +45,16 @@ input.addEventListener('keypress', (event) => {
 
 // Removing an item from the list
 import removeTask from './modules/removeTask';
-const deleteIcon = document.querySelectorAll('.delete-icon');
-deleteIcon.forEach((icon) => {
-  icon.addEventListener('click', (event) => {
-    removeTask(tasks, event);
-    viewTasks(tasks);
+document.addEventListener('click', (event) => {
+  const deleteIcons = document.querySelectorAll('.delete-icon');
+  deleteIcons.forEach((icon, index) => {
+    if (event.target === icon) {
+      removeTask(tasks, event);
+      viewTasks(tasks);
+    }    
   })
 })
+
 
 // Edit task
 import editTask from './modules/editTask';
--- src/modules/removeTask.js
+++ src/modules/removeTask.js
@@ -1,6 +1,6 @@
-export default (tasks, event) => {
-    const { index } = event.target.parentNode.dataset;
-    tasks.splice(index, 1);
+export default (tasks, index) => {
+    console.log(index);
+    tasks.splice(3, 1);
     for(let i = index; i <= tasks.length; i++) {
         tasks[i].index = i;
     }

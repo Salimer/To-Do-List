96dfda53d6dcd606638534f388c86f64f35c4508
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-05 10:39:47 +0300

    Fix webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,8 @@ module.exports = {
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
     clean: true,
   },
   optimization: {

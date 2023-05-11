1a8f604f6fee7c2b5d71c41ef76a91c1fbf77cc8
unorji kingsley <unorjikingsley@gmail.com>
2023-05-11 14:33:15 +0100

    Fix linters error

--- src/Test2.test.js
+++ src/Test2.test.js
@@ -7,15 +7,15 @@ import list from './modules/list.js';
 import clearCompletedTasks from './modules/clearCompletedTasks.js';
 
 describe('Edit task', () => {
-    let tasks = [];
-    const newTask = {
-        description: "New task",
-        completed: false,
-        id: tasks.length + 1
-    }
+  let tasks = [];
+  const newTask = {
+    description: 'New task',
+    completed: false,
+    id: tasks.length + 1,
+  };
 
-    test('Clear completed test', () => {
-        document.body.innerHTML = `
+  test('Clear completed test', () => {
+    document.body.innerHTML = `
             <ul class="to-do-list">
             <li id="to-do-title" class="list-item">Today's To Do</li>
             <li class="list-item">
@@ -25,39 +25,40 @@ describe('Edit task', () => {
         <a class="clear">Clear all completed</a>
         `;
 
-        // pushing new tasks into the array
-        for (let i = 0; i < 8; i++) {
-            const newTask = { description: `Task ${i}`, completed: i % 2 !== 0 };
-            tasks.push(newTask);
-        }
-        const container = document.querySelector('.to-do-list');
-        tasks.forEach((task) => {
-            list(container, task, '#');
-        });
-          
-        // Clearing the completed tasks
-        tasks = clearCompletedTasks(tasks);
-        while (container.children[2]) {
-            container.removeChild(container.children[2]);
-        }
-        tasks.forEach((task) => {
-            list(container, task, '#');
-          });
-        const taskItems = document.querySelectorAll('.description');
+    // pushing new tasks into the array
+    for (let i = 0; i < 8; i++) {
+      const newTask = { description: `Task ${i}`, completed: i % 2 !== 0 };
+      tasks.push(newTask);
+    }
 
-        // The test where there were 8 tasks, then deleted 4 completed tasks
-        expect(taskItems).toHaveLength(4);
-    })
+    const container = document.querySelector('.to-do-list');
+    tasks.forEach((task) => {
+      list(container, task, '#');
+    });
 
-    test('Toggle completed status', () => {
-        newTask.completed = false;
-        tasks.push(newTask);
-        taskStatusUpdate(tasks, 0);
-        expect(tasks[0].completed).toBe(true);
-    })
-    test('Edit task and update tasks array', () => {      
-        tasks.push(newTask);
-        editTask(tasks, 0, 'Go shopping');
-        expect(tasks[0].description).toBe('Go shopping');
-    })
-})
+    // Clearing the completed tasks
+    tasks = clearCompletedTasks(tasks);
+    while (container.children[2]) {
+      container.removeChild(container.children[2]);
+    }
+    tasks.forEach((task) => {
+      list(container, task, '#');
+    });
+    const taskItems = document.querySelectorAll('.description');
+
+    // The test where there were 8 tasks, then deleted 4 completed tasks
+    expect(taskItems).toHaveLength(4);
+  });
+
+  test('Toggle completed status', () => {
+    newTask.completed = false;
+    tasks.push(newTask);
+    taskStatusUpdate(tasks, 0);
+    expect(tasks[0].completed).toBe(true);
+  });
+  test('Edit task and update tasks array', () => {
+    tasks.push(newTask);
+    editTask(tasks, 0, 'Go shopping');
+    expect(tasks[0].description).toBe('Go shopping');
+  });
+});

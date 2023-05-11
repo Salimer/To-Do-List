3449ee57683a907324738d451c354b7c156e5fde
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-11 15:12:25 +0300

    Add tests for edit tasks

--- src/edit&clear.test.js
+++ src/edit&clear.test.js
@@ -2,28 +2,63 @@
  * @jest-environment jsdom
  */
 import editTask from './modules/editTask.js';
+import taskStatusUpdate from './modules/taskStatusUpdate.js';
+// import viewTasks from './modules/viewTasks.js';
+const viewTasks = require('./modules/viewTasks')
 
 describe('Edit task', () => {
-    const tasks = [
-        {
-            description: 'task1',
-            completed: false,
-            id: 0
-        }
-    ]
-    test('Edit task and update tasks array', () => {
+    const tasks = [];
+    const newTask = {
+        description: "New task",
+        completed: false,
+        id: tasks.length + 1
+    }
+
+    test('Clear completed test', () => {
         document.body.innerHTML = `
-        <li class="list-item">
-            <input class="check-box" type="checkbox" unchecked="">
-            <p class="description" contenteditable="true">${tasks[0].description}</p>
-            <a class="delete-icon">
-                <img class="delete-img" src="http://127.0.0.1:3000/dist/940f2754b6cc078901bdf1f87308209e.svg" alt="trash">
-            </a>
-        </li>
+            <ul class="to-do-list">
+            <li id="to-do-title" class="list-item">Today's To Do</li>
+            <li class="list-item">
+                <input class="input" type="text" placeholder="Add to your list...">
+            </li>
+            </ul>
+        <a class="clear">Clear all completed</a>
         `;
-
-        // const description = document.
-
+        for (let i = 0; i < 8; i++) {
+            const newTask = { description: `Task ${i}`, completed: i % 2 !== 0 };
+            tasks.push(newTask);
+        }
+        const container = document.querySelector('.to-do-list');
+        tasks.forEach((task) => {
+            const listItem = document.createElement('li');
+            listItem.className = 'list-item';
         
+            const input = document.createElement('input');
+            input.setAttribute('class', 'check-box');
+            input.setAttribute('type', 'checkbox');
+            input.setAttribute('checked', true);
+            listItem.appendChild(input);
+            listItem.innerHTML = `
+              <input class="check-box" type="checkbox" ${task.completed ? 'checked' : 'unchecked'}>
+              <p class="description" contenteditable="true">${task.description}</p>
+              <a class="delete-icon"><img class="delete-img" src="#" alt="trash"></a>
+              `;
+            container.appendChild(listItem);
+          });
+        const list = document.querySelectorAll('.description');
+
+        console.log(list.length);
+    })
+
+    test('Toggle completed status', () => {
+        newTask.completed = false;
+        tasks.push(newTask);
+        taskStatusUpdate(tasks, 0);
+        expect(tasks[0].completed).toBe(true);
+    })
+    test('Edit task and update tasks array', () => {      
+        tasks.push(newTask);
+        editTask(tasks, 0, 'Go shopping');
+        expect(tasks[0].description).toBe('Go shopping');
     })
 })

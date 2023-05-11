9177bf2940d85a08a986abd23724ae34cb813064
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-11 15:37:03 +0300

    Modify viewTasks.js

--- src/modules/viewTasks.js
+++ src/modules/viewTasks.js
@@ -1,5 +1,7 @@
-import trash from '../trash.svg';
+import trashIcon from '../trash.svg';
+import list from './list.js';
 
+const trash = trashIcon;
 export default (tasks) => {
   const container = document.querySelector('.to-do-list');
   while (container.children[2]) {
@@ -10,20 +12,7 @@ export default (tasks) => {
   tasks.sort((a, b) => a.index - b.index);
 
   tasks.forEach((task) => {
-    const listItem = document.createElement('li');
-    listItem.className = 'list-item';
-
-    const input = document.createElement('input');
-    input.setAttribute('class', 'check-box');
-    input.setAttribute('type', 'checkbox');
-    input.setAttribute('checked', true);
-    listItem.appendChild(input);
-    listItem.innerHTML = `
-      <input class="check-box" type="checkbox" ${task.completed ? 'checked' : 'unchecked'}>
-      <p class="description" contenteditable="true">${task.description}</p>
-      <a class="delete-icon"><img class="delete-img" src="${trash}" alt="trash"></a>
-      `;
-    container.appendChild(listItem);
+    list(container, task, trash);
   });
 
   localStorage.setItem('toDoList', JSON.stringify(tasks));
